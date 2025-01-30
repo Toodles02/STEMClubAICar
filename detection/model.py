826c0df6e03b02e44f2c95bbd559533b648a89d7
Rohan Brar <rohanbrar09@gmail.com>
2025-01-29 20:21:57 -0800

    Initial Commit

---
+++ detection/model.py
@@ -0,0 +1,14 @@
+import torch.nn as nn 
+
+
+class TrafficSignDetector(nn.Module):
+    def __init__(self):
+        self.conv_layer = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3),
+            nn.ReLU,
+            nn.MaxPool2d(2, 2),
+            #TODO 
+        )
+    def forward(self, x):
+        #TODO 
+        return x 
