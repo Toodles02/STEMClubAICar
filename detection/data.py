faa545edefa6762712f3cfdd1bf213a125699795
Rohan <rohanbrar09@gmail.com>
2025-02-18 20:55:33 -0800

    Prepared dataset

--- detection/data.py
+++ detection/data.py
@@ -9,13 +9,6 @@ from PIL import Image
 
 PATH = "D:/Rohan Stuff/Datasets/Traffic_Dataset/annotations/"
 
-
-def remove_image(image_folder, image_key): 
-    image_path = os.path.join(image_folder, f"{image_key}.jpg")
-    if os.path.exists(image_path):
-        os.remove(image_path)
-        print(f"DELETED -------> {image_key}")
-
 # 347 Labels, 42k images 
 
 class TrafficSignDataset(Dataset):
@@ -51,24 +44,23 @@ image_folder = 'images'
 annotations_folder = 'mtsd_v2_fully_annotated/annotations' 
 
 
-if __name__ == '__main__':
 
-    mean = [0.4270, 0.4716, 0.4838]
-    std = [0.2461, 0.2567, 0.2902] 
-    transform = transforms.Compose([
-    transforms.Resize((128, 128)),
-    transforms.ToTensor(), 
-    transforms.Normalize(mean, std)
-    ])
+mean = [0.4270, 0.4716, 0.4838]
+std = [0.2461, 0.2567, 0.2902] 
 
-    dataset = TrafficSignDataset(PATH + image_folder, PATH + annotations_folder, transform=transform)
-    print(len(dataset))
+transform = transforms.Compose([
+transforms.Resize((128, 128)),
+transforms.ToTensor(), 
+transforms.Normalize(mean, std)
+])
 
-    sample_size = 5000 
+dataset = TrafficSignDataset(PATH + image_folder, PATH + annotations_folder, transform=transform)
 
-    indices = random.sample(range(len(dataset)), sample_size)
+sample_size = 5000 
 
-    sample = Subset(dataset, indices)
+indices = random.sample(range(len(dataset)), sample_size)
+
+sample = Subset(dataset, indices)
 
 
 
--- detection/test.py
+++
@@ -1,6 +0,0 @@
-import torch
-import numpy as np 
-
-dim1_tensor = torch.randn(3) 
-print(dim1_tensor.shape)
-print(torch.dim(dim1_tensor, 0))
